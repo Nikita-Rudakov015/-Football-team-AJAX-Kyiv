72c402079d66a535bf3ebb73ea57727e78e782e1
mandarin <nikita.rudakov.015@gmail.com>
2022-08-09 15:07:19 +0300

    Creation Domain, Footballer.Application, Persistense, WEBApi

---
+++ AJAX-Kyiv/AJAX-Kyiv.Domain/Footballer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AJAX_Kyiv.Domain
+{
+    public class Footballer
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int number { get; set; }
+        public string position { get; set; }
+
+    }
+}
